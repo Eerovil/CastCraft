9a33582b2cbcc6730c681d9198319fbc9b05e3ca
Eero Vilpponen <eero.vilpponen@gmail.com>
2023-06-11 06:37:24 +0000

    Add sentry

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,25 @@ import { EntityMap } from './moreTypes'
 import { startTouchInput } from './touchInput'
 import { initializeInventory } from './inventory.ts'
 import { Item } from './apiTypes.ts'
+import * as Sentry from "@sentry/browser";
+
+
+Sentry.init({
+  dsn: "https://632d9591067c4f76a1a51e42ec9b75e5@o4505339492433920.ingest.sentry.io/4505339494989824",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/ahdintie.duckdns\.org\/castcraft/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
