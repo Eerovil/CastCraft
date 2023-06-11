d568119f3edff9cfc3c5f18379d2faaefb0887c6
Eero Vilpponen <eero.vilpponen@gmail.com>
2023-06-11 06:39:10 +0000

    Build sentry

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -24,8 +24,6 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
-myUndefinedFunction();
-
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
