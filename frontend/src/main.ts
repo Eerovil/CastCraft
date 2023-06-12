162bdedd81f28edc9ddb7f3a8de761a98b4883e0
Eero Vilpponen <eero.vilpponen@gmail.com>
2023-06-12 05:51:01 +0000

    MAybe fix console log performance

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -25,6 +25,9 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
+if(typeof console === "undefined"){
+  (console as any) = {};
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
