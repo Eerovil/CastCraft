2c8af2fd95ad0b870c6427685befa3444e319bf8
Eero Vilpponen <eero.vilpponen@gmail.com>
2023-06-12 05:48:59 +0000

    Prevent holding while carrying

--- backend/inventory.py
+++ backend/inventory.py
@@ -12,6 +12,15 @@ def select_item(request_sid: str, slug: str):
     player_entity = get_player_entity_from_request(request_sid)
     if player_entity is None:
         return
+
+    if player_entity.carrying_entity_id:
+        logger.info(f"Player {player_entity.id} tried to select item {slug} but they are already carrying")
+        player_entity.holding = None
+        player_entity.update_sprites()
+        entity_db = get_entity_db()
+        entity_db[player_entity.id] = player_entity
+        return [], [player_entity]
+
     for item in player_entity.inventory.items:
         if item.slug == slug:
             player_entity.holding = item
